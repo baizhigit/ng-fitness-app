281682b49b9727c037d06f57b55200431f1bb4cd
Koblandy <kbaizhigit@outlook.com>
2019-08-08 18:22:38 +0600

    Initial commit

--- apps/fitness-app-e2e/src/integration/app.spec.ts
+++ apps/fitness-app-e2e/src/integration/app.spec.ts
@@ -1,9 +1,9 @@
 import { getGreeting } from '../support/app.po';
 
-describe('ng-fitness', () => {
+describe('fitness-app', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    getGreeting().contains('Welcome to ng-fitness!');
+    getGreeting().contains('Welcome to fitness-app!');
   });
 });
--- apps/fitness-app/src/app/app.component.ts
+++ apps/fitness-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'myworkspace-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-fitness';
+  title = 'fitness-app';
 }
